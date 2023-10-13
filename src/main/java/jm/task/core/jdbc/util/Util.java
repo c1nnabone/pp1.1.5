2ff365f8a17561eefd5442d6437ac167e95b7585
c1nnabone <skovorodaplay@gmail.com>
2023-10-13 17:39:52 +0300

    done

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,7 +10,7 @@ public class Util {
     private static final String username = "root";
     private static final String password = "root";
     public static Connection Connect() {
-        try {
+        try (Connection connection = DriverManager.getConnection(url, username, password)){
             return DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
